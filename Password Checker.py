d34fc2ab34686d4bd645daa91953971cc76d0490
brax-vesh <158243067+brax-vesh@users.noreply.github.com>
2024-06-25 22:36:30 +1000

    finalised project

--- Password Checker.py
+++ Password Checker.py
@@ -8,37 +8,27 @@ def letter_searcher(text):
     text = password.text
     for char in text:
         if char.isalpha():
-            print('letter found')
             return True
 
 def dictionary_searcher(text):
     text = password.text
     dictionary = PyDictionary()
     if dictionary.meaning(text,True) is None:
-        print(f"'{text}' is not found in the dictionary.")
         return False
     else:
-        print(f"'{text}' Is found in the dictionary")
         return True
-
-def password_text(text):
-    text = password.text
-    print(f'this is the password: {text}') 
 
 def password_length(text):
     text = password.text
     if len(text) < 12:
-        print("Password too short")
         return False
     elif len(text) >= 12:
-        print("Password length good")
         return True
 
 def uppercase_searcher(text):
     text = password.text
     for char in text:
         if char.isupper():
-            print('uppercase found')
             return True
         else:
             print('no uppercase')
@@ -47,7 +37,6 @@ def lowercase_searcher(text):
     text = password.text
     for char in text:
         if char.islower():
-            print('lowercase found')
             return True
         else:
             print('no uppercase')
@@ -56,7 +45,6 @@ def number_searcher(text):
     text = password.text
     for char in text:
         if char.isnumeric():
-            print('number found')
             return True
         else:
             print('no number')
@@ -67,7 +55,6 @@ def common_password_searcher(text):
         for line in file:
             line = line.strip()
             if line == text:
-                print('password is extremely common')
                 return True
             else:
                 print('not common')
@@ -78,7 +65,6 @@ def breached_password_searcher(text):
         for line in file:
             line = line.strip()
             if line == text:
-                print('password has been found in a data breach')
                 return True
             else:
                 print('not breached')
@@ -87,7 +73,6 @@ def symbol_searcher(text):
     text = password.text
     for char in text:
         if not char.isalnum():
-            print(f"{char} is symbol")
             return True
         else:
             print('no symbol')
@@ -100,142 +85,156 @@ def symbol_searcher(text):
 
 # Suggestions
 def suggestions(text):
+        text = password.text
 
-    letter_found = letter_searcher(text)
-    password_in_dict = dictionary_searcher(text)
-    good_length = password_length(text)
-    uppercase_letter_found = uppercase_searcher(text)
-    lowercase_letter_found = lowercase_searcher(text)
-    number_found = number_searcher(text)
-    password_common = common_password_searcher(text)
-    password_breached = breached_password_searcher(text)
-    symbol_found = symbol_searcher(text)
+        # dictionary_on = dict_check_validation(text)
+        # if dictionary_on == True:
+        
+        password_in_dict = dictionary_searcher(text)
+        letter_found = letter_searcher(text)
+        good_length = password_length(text)
+        uppercase_letter_found = uppercase_searcher(text)
+        lowercase_letter_found = lowercase_searcher(text)
+        number_found = number_searcher(text)
+        password_common = common_password_searcher(text)
+        password_breached = breached_password_searcher(text)
+        symbol_found = symbol_searcher(text)
 
-    suggestion_group1 = []
-    suggestion_group2 = []
-    suggestion_group3 = []
+        suggestion_group1 = []
+        suggestion_group2 = []
+        suggestion_group3 = []
 
-    suggestions1_amount = 0
-    suggestions2_amount = 0
-    suggestions3_amount = 0
+        suggestions1_amount = 0
+        suggestions2_amount = 0
+        suggestions3_amount = 0
 
-    points_bar.value = 0
-
-    password_score = 0
+        password_score = 0
 
     # check for length over 12 characters
         
-    if good_length == True:
-        password_score += 1
-    else:
-        suggestion_group2.append('to be longer')
-        suggestions2_amount += 1
+        if good_length == True:
+            password_score += 1
+        else:
+            suggestion_group2.append('to be longer')
+            suggestions2_amount += 1
 
     # check for letter
 
-    if letter_found == True:
-        password_score += 1
-    else:
-        suggestions1_amount += 1
+        if letter_found == True:
+            password_score += 1
+        else:
+            suggestions1_amount += 1
 
 
     # check for uppercase    
-    if symbol_found == True:
-        password_score += 1
-    else:
-        suggestion_group2.append('a symbol')
-        suggestions2_amount += 1
+        if symbol_found == True:
+            password_score += 1
+        else:
+            suggestion_group2.append('a symbol')
+            suggestions2_amount += 1
 
 
-    if uppercase_letter_found == True:
-        password_score += 1
-    else:
-        suggestion_group1.append('an uppercase letter')
-        suggestions1_amount += 1
+        if uppercase_letter_found == True:
+            password_score += 1
+        else:
+            suggestion_group1.append('an uppercase letter')
+            suggestions1_amount += 1
 
 
     # check for lowercase
     
-    if lowercase_letter_found == True:
-        password_score += 1
-    else:
-        suggestion_group1.append('a lowercase letter')
-        suggestions1_amount += 1
+        if lowercase_letter_found == True:
+            password_score += 1
+        else:
+            suggestion_group1.append('a lowercase letter')
+            suggestions1_amount += 1
 
 
     # check for number
     
-    if number_found == True:
-        password_score += 1
-    else:
-        suggestion_group2.append('a number')
-        suggestions2_amount += 1
+        if number_found == True:
+            password_score += 1
+        else:
+            suggestion_group2.append('a number')
+            suggestions2_amount += 1
 
     # breached, dictionary words and common passwords result in a 0 score
     
-    if password_breached == True:
-        suggestion_group3.append('compromised')
-        suggestions3_amount += 1
+        if password_breached == True:
+            suggestion_group3.append('compromised')
+            suggestions3_amount += 1
 
-    if password_common == True:
-        suggestion_group3.append('too common')
-        suggestions3_amount += 1
+        if password_common == True:
+            suggestion_group3.append('too common')
+            suggestions3_amount += 1
 
-    if password_in_dict == True:
-        suggestion_group3.append('in the english dictionary')
-        suggestions3_amount += 1
+        if password_in_dict == True:
+            suggestion_group3.append('in the english dictionary')
+            suggestions3_amount += 1
+
+        # if dictionary_on == True:
+        #     if password_in_dict == True and len(text) >= 1:
+        #         suggestion_group3.append('in the english dictionary')
+        #         suggestions3_amount += 1
+
+        #     if password_in_dict == True:
+        #         password_score = 0
+        #     else:
+        #         print('pass not in dict')
+        # else:
+        #     print('password_disabled')
     
-    if password_breached or password_common or password_in_dict == True:
-        password_score = 0
+        if password_breached or password_common or password_in_dict == True:
+            password_score = 0
 
-    if password_breached and password_common and password_in_dict == False:
-        password_score += 1
+        if password_breached and password_common and password_in_dict == False:
+            password_score += 1
 
-    if password_score == 0:
-        points_bar.value = 0
-        rating.text = 'Password is unusable'
-    elif password_score == 1:
-        points_bar.value = 20
-    elif password_score == 2:
-        points_bar.value = 20
-        rating.text = 'Password is terrible'
-    elif password_score == 3:
-        points_bar.value = 40
-        rating.text = 'Password is weak'
-    elif password_score == 4:
-        points_bar.value = 60
-        rating.text = 'Password is mediocre'
-    elif password_score == 5:
-        points_bar.value = 80
-        rating.text = 'Password is decent'
-    else:
-        points_bar.value = 100
-        rating.text = 'Password is strong'
+        if password_score == 0:
+            star_rating.text = '☆☆☆☆☆'
+            rating.text = '" Password is unusable "'
+        elif password_score == 1:
+            star_rating.text = '★☆☆☆☆'
+        elif password_score == 2:
+            star_rating.text = '★☆☆☆☆'
+            rating.text = '" Password is terrible "'
+        elif password_score == 3:
+            star_rating.text = '★★☆☆☆'
+            rating.text = '" Password is weak "'
+        elif password_score == 4:
+            star_rating.text = '★★★☆☆'
+            rating.text = '" Password is mediocre "'
+        elif password_score == 5:
+            star_rating.text = '★★★★☆'
+            rating.text = '" Password is decent "'
+        else:
+            star_rating.text = '★★★★★'
+            rating.text = '" Password is strong "'
 
-    suggestion_words1 = ', '.join(suggestion_group1)
+        suggestion_words1 = ', '.join(suggestion_group1)
 
-    if suggestions1_amount >= 1:
-        suggestion1.text = f'Password should include: {suggestion_words1}'
-    else:
-        suggestion1.text = ''
+        if suggestions1_amount >= 1:
+            suggestion1.text = f'Password should include: {suggestion_words1}'
+        else:
+            suggestion1.text = ''
 
 
-    suggestion_words2 = ', '.join(suggestion_group2)
+        suggestion_words2 = ', '.join(suggestion_group2)
 
-    if suggestions2_amount >= 1:
-        suggestion2.text = f'Password needs: {suggestion_words2}'
-    else:
-        suggestion2.text = ''
+        if suggestions2_amount >= 1:
+            suggestion2.text = f'Password needs: {suggestion_words2}'
+        else:
+            suggestion2.text = ''
 
-    suggestion_words3 = ', '.join(suggestion_group3)
+        suggestion_words3 = ', '.join(suggestion_group3)
     
-    if suggestions3_amount >= 1:
-        suggestion3.text = f'Uh oh! Password is: {suggestion_words3}'
-    else:
-        suggestion3.text = ''
+        if suggestions3_amount >= 1:
+            suggestion3.text = f'Warning! Password is: {suggestion_words3}'
+        else:
+            suggestion3.text = ''
 
-    print(password_score)
-    print(suggestion_group1)
+        print(password_score)
+        print(suggestion_group1)
         
 
     # if len(password.text) < 12:
@@ -252,7 +251,7 @@ def suggestions(text):
 
 # App Grid
 
-app = gp.GooeyPieApp('Password Strength Evaluator')
+app = gp.GooeyPieApp('Password Strength Evaluator!')
 app.width = 1000
 app.height = 500
 app.set_grid(6,5)
@@ -267,7 +266,8 @@ app.set_grid(6,5)
 app_name = gp.StyleLabel(app, "Password Strength Evaluator")
 app_name.font_style = 'italic'
 app.add(app_name, 1, 1, align='left')
-
+app_name.font_size = 40
+app_name.font_name = 'Times New Roman'
 
 
 
@@ -277,7 +277,7 @@ app.add(app_name, 1, 1, align='left')
 password = gp.Secret(app)
 password.width = 30
 password.text = ('Enter Your Password Here!')
-app.add(password, 3 ,4, align='center', valign='top')
+app.add(password, 3 ,4, align='center', valign='bottom')
 
 
 
@@ -288,20 +288,21 @@ app.add(password, 3 ,4, align='center', valign='top')
 def toggle_password_visibility(event):
     password.toggle()
 
+# password_toggle = gp.ImageButton(app, None, toggle_password_visibility, 'Hide Password')
 password_toggle = gp.Checkbox(app, 'Hide Password')
 password_toggle.add_event_listener('change', toggle_password_visibility)
-
 password.toggle()
-app.add(password_toggle, 3, 5, align='left', valign='top')
 
-
-
+app.add(password_toggle, 3, 5, align='left', valign='bottom')
 
 
 # Suggestions Name (Text)
 
 suggestions_name = gp.StyleLabel(app, "Suggestions:")
-app.add(suggestions_name, 2, 1, align='left')
+suggestions_name.font_style = 'italic'
+suggestions_name.font_size = 25
+suggestions_name.font_name = 'Times New Roman'
+app.add(suggestions_name, 2, 1, align='left', valign='bottom')
 
 
 
@@ -328,22 +329,26 @@ def open_other_window(event):
 tip_window = gp.Window(app, 'Tips For a Good Password')
 tip_window.width = 700
 tip_window.height = 500
-tip_window.set_grid(5, 5)
+tip_window.set_grid(10, 5)
 
-the_tips = gp.Label(tip_window, "Here I have compiled a few tips for creating a strong password:")
-tip_title = gp.Label(tip_window, 'Here i have compiled some tips for a good password!')
-tip_paragraph = gp.Label(tip_window, "")
-def tip_text():
-    with open('tip_para.txt', 'r') as file:
-            for line in file:
-                line = line.strip()
-            tip_paragraph.text = line
+tip_title = gp.StyleLabel(tip_window, 'Here i have compiled some tips for a good password:')
+tip_title.font_name = 'Times New Roman'
+tip_title.font_size = 25
+tips1 = gp.Label(tip_window, "1) Include an uppercase and lowercase letter in the password")
+tips2 = gp.Label(tip_window, "2) Include a number and symbol in your password")
+tips3 = gp.Label(tip_window, "3) A secure password should be 12 characters long")
+tips4 = gp.Label(tip_window, "4) A password should not be a dictionary word")
+tips5 = gp.Label(tip_window, "5) Our program will warn you if you password has been breached or is too common. ")
 
-tip_text()
+# tips6 = gp.Label(tip_window, "(Tick the 'Disable Dictionary' box to reduce computer lag. Warning: this will reduce password strength.)")
 
-tip_window.add(the_tips, 3, 1)
 tip_window.add(tip_title, 1, 1)
-tip_window.add(tip_paragraph, 2, 1)
+tip_window.add(tips1, 2, 1)
+tip_window.add(tips2, 3, 1)
+tip_window.add(tips3, 4, 1)
+tip_window.add(tips4, 5, 1)
+tip_window.add(tips5, 6, 1)
+# tip_window.add(tips6, 10, 1, align='center')
 
 tip_btn = gp.Button(app, 'Tips', open_other_window)
 tip_btn.width = 3
@@ -355,40 +360,49 @@ app.add(tip_btn, 6, 5, align='right', valign='bottom')
 
 # Rating
 
-rating = gp.Label(app, '')
-app.add(rating, 5, 4, align='center', valign='top')
+rating = gp.Label(app, '"  Here your password will be rated out of five stars!  "')
+app.add(rating, 2, 4, align='center', valign='top')
 
 
 
 
 
-# Points Bar
+# Star rating
 
-points_bar = gp.Progressbar(app)
-points_bar.width = 270
-app.add(points_bar, 4, 4, align='center')
+star_rating = gp.Label(app, '☆☆☆☆☆')
+app.add(star_rating, 1, 4, align='center', valign='bottom')
 
 
 
 
 
 #Check Button
-
 check_button = gp.Button(app, 'Check Strength', suggestions)
 check_button.width = 11
-app.add(check_button, 2, 4, align='center', valign='bottom')
+app.add(check_button, 4, 4, align='center', valign='top')
 
 
+# Disable Dictionary
 
+# def dict_check_validation(event):
+#     if dis_dict.checked:
+#         return False
+#     else:
+#         return True
+
+# dis_dict = gp.Checkbox(app, 'Disable Dictionary Check')
+# password_toggle.add_event_listener('change', dict_check_validation)
+
+# app.add(dis_dict, 6,4, align='right', valign='bottom')
 
 
 # Suggestions
 
 suggestion1 = gp.Label(app, '')
-app.add(suggestion1, 3,1, align='left', valign='top')
+app.add(suggestion1, 3,1, align='left', valign='bottom')
 
 suggestion2 = gp.Label(app, '')
-app.add(suggestion2, 4,1, align='left', valign='top')
+app.add(suggestion2, 4,1, align='left', valign='middle')
 
 suggestion3 = gp.Label(app, '')
 app.add(suggestion3, 5,1, align='left', valign='top')
