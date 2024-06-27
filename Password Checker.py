8434d8cbe3183547e6cb379bf0ec180ac53a2363
brax-vesh <158243067+brax-vesh@users.noreply.github.com>
2024-06-27 10:30:34 +1000

    removed unnecessary comments and polished up suggestion function changed length function so that it now tells you if the string is long enough but also won't allow you to just enter 12 spaces and get 'good length'

--- Password Checker.py
+++ Password Checker.py
@@ -125,9 +125,10 @@ def suggestions(text):
         if good_length == True:
             if only_space == False:
                 password_score += 1
-            else:
-                suggestion_group2.append('to be longer')
-                suggestions2_amount += 1
+                suggestion_group2.append('good length')
+        else:
+            suggestion_group2.append("not long enough")
+            suggestions2_amount += 1
 
     # check for letter
 
@@ -137,14 +138,15 @@ def suggestions(text):
             suggestions1_amount += 1
 
 
-    # check for uppercase    
+    # check for symbol
+               
         if symbol_found == True:
             password_score += 1
         else:
-            suggestion_group2.append('to contain a symbol (!,@,#,$,?)')
+            suggestion_group2.append('missing a symbol (!,@,#,$,?)')
             suggestions2_amount += 1
 
-
+    # check for uppercase    
         if uppercase_letter_found == True:
             password_score += 1
         else:
@@ -166,7 +168,7 @@ def suggestions(text):
         if number_found == True:
             password_score += 1
         else:
-            suggestion_group2.append('to have a number')
+            suggestion_group2.append('missing a number')
             suggestions2_amount += 1
 
     # breached, dictionary words, only spaces and common passwords result in a 0 score
@@ -190,18 +192,6 @@ def suggestions(text):
             suggestion_group3.append('comprised of only spaces, please use other characters')
             suggestions3_amount += 1
 
-        # if dictionary_on == True:
-        #     if password_in_dict == True and len(text) >= 1:
-        #         suggestion_group3.append('in the english dictionary')
-        #         suggestions3_amount += 1
-
-        #     if password_in_dict == True:
-        #         password_score = 0
-        #     else:
-        #         print('pass not in dict')
-        # else:
-        #     print('password_disabled')
-    
         if password_breached or password_common or password_in_dict and only_space == True:
             password_score = 0
 
@@ -240,7 +230,7 @@ def suggestions(text):
         suggestion_words2 = ' AND '.join(suggestion_group2)
 
         if suggestions2_amount >= 1:
-            suggestion2.text = f'Password needs: {suggestion_words2}'
+            suggestion2.text = f'Password is: {suggestion_words2}'
         else:
             suggestion2.text = ''
 
@@ -252,20 +242,7 @@ def suggestions(text):
             suggestion3.text = ''
 
         print(password_score)
-        print(suggestion_group1)
-        
-
-    # if len(password.text) < 12:
-    #     suggestion.text="to short"
-    # elif len(password.text) >= 12:
-    #      suggestion.text="Y suggestion"
-    # else:
-    #     suggestion.text="X suggestion"
-
-
-    
-
-
+        print(suggestion_group1)        
 
 # App Grid
 
@@ -316,7 +293,7 @@ app.add(password_toggle, 3, 5, align='left', valign='bottom')
 
 # Suggestions Name (Text)
 
-suggestions_name = gp.StyleLabel(app, "Suggestions:")
+suggestions_name = gp.StyleLabel(app, "Feedback:")
 suggestions_name.font_style = 'italic'
 suggestions_name.font_size = 25
 suggestions_name.font_name = 'Times New Roman'
@@ -341,6 +318,7 @@ app.add(copy_password_button, 4,5, align='left', valign='top')
 
 
 # Tips Button
+
 def open_other_window(event):
     tip_window.show()
 
@@ -352,13 +330,14 @@ tip_window.set_grid(10, 5)
 tip_title = gp.StyleLabel(tip_window, 'Tips For a Good Password:')
 tip_title.font_name = 'Times New Roman'
 tip_title.font_size = 25
+
 tips1 = gp.Label(tip_window, "1) Include an uppercase and lowercase letter in the password")
 tips2 = gp.Label(tip_window, "2) Include a number and symbol in your password")
 tips3 = gp.Label(tip_window, "3) A secure password should be 12 characters long")
 tips4 = gp.Label(tip_window, "4) A password should not be a dictionary word")
 tips5 = gp.Label(tip_window, "5) Our program will warn you if you password has been breached or is too common. ")
 
-# tips6 = gp.Label(tip_window, "(Tick the 'Disable Dictionary' box to reduce computer lag. Warning: this will reduce password strength.)")
+# tip window addition
 
 tip_window.add(tip_title, 1, 1)
 tip_window.add(tips1, 2, 1)
@@ -366,7 +345,6 @@ tip_window.add(tips2, 3, 1)
 tip_window.add(tips3, 4, 1)
 tip_window.add(tips4, 5, 1)
 tip_window.add(tips5, 6, 1)
-# tip_window.add(tips6, 10, 1, align='center')
 
 tip_btn = gp.Button(app, 'Tips', open_other_window)
 tip_btn.width = 3
@@ -395,23 +373,10 @@ app.add(star_rating, 1, 4, align='center', valign='bottom')
 
 
 #Check Button
+
 check_button = gp.Button(app, 'Check Strength', suggestions)
 check_button.width = 11
 app.add(check_button, 4, 4, align='center', valign='top')
-
-
-# Disable Dictionary
-
-# def dict_check_validation(event):
-#     if dis_dict.checked:
-#         return False
-#     else:
-#         return True
-
-# dis_dict = gp.Checkbox(app, 'Disable Dictionary Check')
-# password_toggle.add_event_listener('change', dict_check_validation)
-
-# app.add(dis_dict, 6,4, align='right', valign='bottom')
 
 
 # Suggestions
@@ -429,18 +394,4 @@ app.add(suggestion3, 5,1, align='left', valign='top')
 
 app.run()
 
-# suggestions to implement
-# 1) length must be greater than 10
 
-# 2) 1 capital letter and 1 lower case letter must be used
-# 3) a special character must be used
-
-# 4) password shouldn't be a dictionary word
-# 5) shouldn't be a name
-
-# 6) password must contain a number
-# 7) password shouldn't contain numbers in succession e.g (1, 2, 3, 4, 5)
-# 5) shouldn't be a name
-
-# 6) password must contain a number
-# 7) password shouldn't contain numbers in succession e.g (1, 2, 3, 4, 5)
